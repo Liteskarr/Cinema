be146973cc329f9145d6818b9f668453d9eada07
Liteskarr <398501z3@gmail.com>
2020-12-24 14:06:47 +0300

    feature(ipage)

---
+++ ipage.py
@@ -0,0 +1,21 @@
+"""
+Этот интерфейс позволяет разделить ответственность между несколькими подвиджетами одного приложения.
+"""
+
+
+from PyQt5.QtCore import (pyqtSignal)
+
+
+class IPage:
+    opened_new_page = pyqtSignal()
+
+    def get_new_page(self):
+        raise NotImplementedError()
+
+    def safe_exit(self) -> bool:
+        """
+        Возвращает True, если было совершено закрытие страницы.
+        Иначе False. Требует для контроля, например, за сохранением пользователем изменений.
+        Переопределяется в классах-наследниках.
+        """
+        return True
