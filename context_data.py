42d75cbdbb26c9504d3b0e6781cc09838b42bc46
Liteskarr <398501z3@gmail.com>
2021-01-01 22:58:57 +0300

    refactor(context_data.py)

--- context_data.py
+++ context_data.py
@@ -6,5 +6,7 @@ from dataclasses import dataclass
 @dataclass(init=False)
 class ContextData:
     permission_lvl: int
+    current_cinema: int
+    current_hall: int
 
     connection: sqlite3.Connection
